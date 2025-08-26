8050d89f5e975f66eee23140398e410fcfccf1c5
Hiroshi Uyama <hiroshi@uyama.coffee>
2025-08-27 00:40:27 +0900

    コードを移植

---
+++ app/Views/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>blogkan CDN</title>
+</head>
+<body>
+  <h1>blogkan CDN</h1>
+</body>
+</html>
---
+++ app/Views/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>CDN Proxy Test</title>
+</head>
+<body>
+  <h1>CDN Proxy テスト</h1>
+  <form method="get" action="/">
+    <label>取得したいURL: <input type="text" name="url" size="60" value="https://ai-gazoukan.com/wp/wp-content/uploads/2025/08/00000-3199285662.jpg"></label>
+    <button type="submit">取得</button>
+  </form>
+</body>
+</html>
